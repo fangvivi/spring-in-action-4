6ca15e65de4bd55cf93be620a624fbe18934234b
wayne <fangvivi@qq.com>
2021-12-08 22:37:05 +0800

    chapter03，使用@Profile为不同的开发环境配置bean

--- chapter03/src/test/java/profile_test/DataSourceConfigTest.java
+++ chapter03/src/test/java/profile_test/DataSourceConfigTest.java
@@ -5,20 +5,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.sql.DataSource;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
-import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.*;
 
+/**
+ * 测试Profile功能
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {DataSourceConfig.class})
 @ActiveProfiles("dev")
@@ -33,4 +32,54 @@ public class DataSourceConfigTest {
         List<String> results = jdbc.query("select id , name from Things", (rs, rowNum) -> rs.getString("name"));
         assertEquals("A", results.get(0));
     }
+
+    @RunWith(SpringJUnit4ClassRunner.class)
+    @ContextConfiguration(classes = DataSourceConfig.class)
+    @ActiveProfiles("prod")
+    public static class ProdDataSourceTest {
+
+        @Autowired
+        private DataSource dataSource;
+
+        @Test
+        public void shouldBeJndiDataSource() {
+            // should be null, because there isn't a datasource configured in JNDI
+            assertNull(dataSource);
+        }
+    }
+
+    @RunWith(SpringJUnit4ClassRunner.class)
+    @ContextConfiguration(locations = "classpath:datasource-config-test.xml")
+    @ActiveProfiles("dev")
+    public static class DevDataSourceTest_XMLConfig {
+
+        @Autowired
+        private DataSource dataSource;
+
+        @Test
+        public void shouldBeEmbeddedDataSource() {
+            assertNotNull(dataSource);
+            JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+            List<String> results = jdbcTemplate.query("select * from Things", (rs, rowNum) -> rs.getString("name"));
+            assertEquals("A", results.get(0));
+        }
+
+    }
+
+    @RunWith(SpringJUnit4ClassRunner.class)
+    @ContextConfiguration(locations = "classpath:datasource-config-test.xml")
+    @ActiveProfiles("prod")
+    public static class ProductionDataSourceTest_XMLConfig {
+
+        @Autowired(required = false)
+        private DataSource dataSource;
+
+        @Test
+        public void shouldBeEmbeddedDataSource() {
+            assertNull(dataSource);
+        }
+
+    }
 }
+
+
